af56d20d25900d114dfc9e3ff6dee809dacde26f
Bogdan Manate <bogdan.manate@gmail.com>
2020-02-03 22:26:20 +0100

    Add tests for health checker

--- src/health-checker.test.ts
+++ src/health-checker.test.ts
@@ -28,17 +28,51 @@ describe('Health Checker', () => {
       targetCell = matrix[1][1];
       targetCell.setActiveStatus(true);
     })
-    it('with two or three neighbors', () => {
+    it('with two or three alive neighbors', () => {
       matrix[0][0].setActiveStatus(true);
       matrix[0][1].setActiveStatus(true);
       
       expect(healthChecker.checkIfCellIsAlive(targetCell, matrix)).toBe(true);
+
+      matrix[2][2].setActiveStatus(true);
+      expect(healthChecker.checkIfCellIsAlive(targetCell, matrix)).toBe(true);
+    })
+
+    it('with zero or one alive neighbors', () => {
+      expect(healthChecker.checkIfCellIsAlive(targetCell, matrix)).toBe(false);
+      matrix[2][2].setActiveStatus(true);
+      expect(healthChecker.checkIfCellIsAlive(targetCell, matrix)).toBe(false);
     })
   })
   
   describe('for dead cell', () => {
-    it('with two or three neighbors', () => {
-      // expect(sum()).toBe(0)
+    beforeEach(() => {
+      
+      matrix = [
+        [new Cell(svg, 0,0),new Cell(svg, 0,1),new Cell(svg, 0,2)],
+        [new Cell(svg, 1,0),new Cell(svg, 1,1),new Cell(svg, 1,2)],
+        [new Cell(svg, 2,0),new Cell(svg, 2,1),new Cell(svg, 2,2)],
+      ]
+      
+    })
+    it('located top left with three neighbors', () => {
+      targetCell = matrix[0][0];
+      matrix[0][1].setActiveStatus(true);
+      matrix[1][0].setActiveStatus(true);
+      expect(healthChecker.checkIfCellIsAlive(targetCell, matrix)).toBe(false);
+      matrix[1][1].setActiveStatus(true);
+      expect(healthChecker.checkIfCellIsAlive(targetCell, matrix)).toBe(true);
+
+    })
+
+    it('located bottom right with three neighbors', () => {
+      targetCell = matrix[2][2];
+      matrix[2][1].setActiveStatus(true);
+      matrix[1][2].setActiveStatus(true);
+      expect(healthChecker.checkIfCellIsAlive(targetCell, matrix)).toBe(false);
+      matrix[1][1].setActiveStatus(true);
+      expect(healthChecker.checkIfCellIsAlive(targetCell, matrix)).toBe(true);
+
     })
   })
 
